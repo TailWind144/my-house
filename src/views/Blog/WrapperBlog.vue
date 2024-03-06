07ca6312080439e786fa9de008720473cf2f1677
TailWind <1796158555@qq.com>
2024-03-06 20:02:55 +0800

    增加目录和锚点

---
+++ src/views/Blog/WrapperBlog.vue
@@ -0,0 +1,13 @@
+<template>
+  <article ref="content">
+    <h1>{{ frontmatter.title }}</h1>
+    <p class="!-mt-6 !opacity-50">{{ frontmatter.date }}</p>
+    <slot></slot>
+  </article>
+</template>
+
+<script setup lang="ts">
+const { frontmatter } = defineProps({ frontmatter: Object })
+</script>
+
+<style scoped></style>
